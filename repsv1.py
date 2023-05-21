2f070ac9a7fd77c861d7bf5ac3dc671c3663a1f8
Calvin Green <calvingreen94@aol.com>
2023-05-21 12:56:25 -0500

    Create repsv1.py

---
+++ repsv1.py
@@ -0,0 +1,110 @@
+from web3.middleware import geth_poa_middleware
+from web3.gas_strategies.time_based import medium_gas_price_strategy
+from eth_account.messages import encode_defunct
+from web3 import Web3
+from web3 import middleware
+import json
+import time
+import numpy as np    
+from faunadb import query as q
+from faunadb.client import FaunaClient
+from faunadb.objects import Ref
+from faunadb.errors import BadRequest, NotFound
+    ######################################################################
+#############################################################################
+######## DAO MAINNET CONTRACT TO FETCH ADDRESS INFORMATION FOR REPUTATION####
+#############################################################################
+
+
+infura_url = "https://mainnet.infura.io/v3/###"
+# infura_url = "https://eth-mainnet.g.alchemy.com/v2/###
+client = FaunaClient(secret="###",domain="db.us.fauna.com")
+
+#	 infura_url = 'wss://ropsten.infura.io/ws/v3/89f69d97c5c44c35959cc4d15c0f0531'
+web3 = Web3(Web3.HTTPProvider(infura_url))
+web3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
+def latestBlock():
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    web3.eth.getBlock('latest')
+    a = web3.eth.getBlock('latest')
+    return a
+
+latestBlock()
+
+OG = '0x1A0F33bBc5c7bA83f490cdB6C13ee50e1C851908'
+abi_OG = '[{"inputs":[{"internalType":"string","name":"_name","type":"string"},{"internalType":"string","name":"_symbol","type":"string"},{"internalType":"string","name":"_initBaseURI","type":"string"},{"internalType":"string","name":"_initNotRevealedUri","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"","type":"address"},{"indexed":false,"internalType":"uint256","name":"","type":"uint256"}],"name":"AddressCalled","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address[]","name":"","type":"address[]"}],"name":"AddressesAdded","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[],"name":"PRICE","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"_mintSingleNFT1","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"addressMintedBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"baseExtension","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"baseURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"burn","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_count","type":"uint256"}],"name":"claimTauros","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getCurrentToken","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"maxMintAmount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"maxSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"notRevealedUri","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bool","name":"_state","type":"bool"}],"name":"pause","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"paused","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"reserveNFTs","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"reveal","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"revealed","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_newBaseExtension","type":"string"}],"name":"setBaseExtension","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_newBaseURI","type":"string"}],"name":"setBaseURI","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_notRevealedURI","type":"string"}],"name":"setNotRevealedURI","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_newPRICE","type":"uint256"}],"name":"setPRICE","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_newmaxMintAmount","type":"uint256"}],"name":"setmaxMintAmount","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"tokenId","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_Id","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_owner","type":"address"}],"name":"walletOfOwner","outputs":[{"internalType":"uint256[]","name":"","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"payable","type":"function"}]'
+abi = json.loads(abi_OG)
+contract_OG = web3.eth.contract(address=OG, abi=abi_OG)	
+
+for i in range(0,contract_OG.functions.totalSupply().call()):
+    # i = i+163 # -> IF OPERATION STOP INCREASE THIS NUMBER TO MATCH THE CURRENT TaurosDAO tokenID for airdrop
+    # print(i)
+    infura_url = "https://mainnet.infura.io/v3/4cb849430aaa4b82bb8360011eb397e9"
+    #	 infura_url = 'wss://ropsten.infura.io/ws/v3/89f69d97c5c44c35959cc4d15c0f0531'
+    web3 = Web3(Web3.HTTPProvider(infura_url))
+    web3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
+    holders = contract_OG.functions.ownerOf(i).call()
+    token = contract_OG.functions.walletOfOwner(holders).call()
+    daos_owned = contract_OG.functions.balanceOf(holders).call()
+
+    print(web3.eth.getBlock('latest'))
+    user_data = {'holders':holders,'TAUROS_owned':daos_owned,'Reputation':(daos_owned*100)//10*.666,'token':token}
+    print(user_data)
+
+
+    data = client.query(q.create(
+                    q.collection('member_info'),
+                    {'data': user_data}
+                ))
+    print(data)
+    
+
+
+    ##################################
+    ### FETCHING BLOCK INFORMATION ###
+    ##################################
+    latestBlock()
+    latestBlock()
+    time.sleep(2)
+
+
+    
